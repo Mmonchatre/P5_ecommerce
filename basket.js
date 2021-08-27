36dc3ef20709e9d1896a72711e002d9b1861b568
MMonchatre <michel.monchatre@gmail.com>
2021-08-27 10:06:56 +0200

    update basket.js

--- basket.js
+++ basket.js
@@ -95,6 +95,7 @@ function deleteArticle () {
                     myNode.textContent = '';
                     basketDisplay();
                     deleteArticle();
+                    
                 }
             })
     }
