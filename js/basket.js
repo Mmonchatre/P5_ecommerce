eeff2f46198eea2d652e5154f63f3a8530280bbc
MMonchatre <michel.monchatre@gmail.com>
2021-09-14 17:57:21 +0200

    update verif des champs

--- js/basket.js
+++ js/basket.js
@@ -237,8 +237,15 @@ function serverOrder () {
     window.addEventListener('load', function() {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
+
         // Loop over them and prevent submission
         var validation = Array.prototype.filter.call(forms, function(form) {
+            if (form.checkValidity() === false) {
+                }
+                form.classList.add('was-validated');
+                if (form.checkValidity() === true) {
+                    serverOrder(); 
+                        }
         form.addEventListener('submit', function(event) {
             //alert(form.checkValidity());
             if (form.checkValidity() === false) {
@@ -260,7 +267,6 @@ function serverOrder () {
 
 
 //Order Confirmation 
-document.getElementById("envoi").onclick = function() {
-}
+//document.getElementById("envoi").onclick = function() {}
     
 
